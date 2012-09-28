d210f8f73a69096e0ca02bbce914b9b67a8b1a63
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2012-09-28 23:55:57 +0200

    twitter-search.py: simple search script

---
+++ twitter-search.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+def usage():
+	print __file__, "<search terms>"
+
+import os, sys
+
+twitter = __import__("twitter-export")
+
+searchTerms = []
+for arg in sys.argv[1:]:
+	if arg.startswith("http://") or arg.startswith("https://"):
+		arg = twitter.resolveShortlink(arg)
+	arg = arg.lower()
+	searchTerms += [arg]
+
+if not searchTerms:
+	usage()
+	sys.exit(1)
+	
+# reset LogFile to avoid wrong dir because of symlinks
+twitter.mydir = os.path.dirname(__file__)
+twitter.LogFile = twitter.mydir + "/twitter.log"
+twitter.loadLog()
+
+for _,twit in sorted(twitter.log.iteritems()):
+	
+	if all([term in twit["text"].lower() for term in searchTerms]):
+		print twit["date"], ":", twit["text"]
