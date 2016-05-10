36c9ba9c7516cf3f9094fdc000da6c697f9a4481
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2016-05-10 09:26:37 +0200

    explicitely use python2 for now

--- twitter-export.py
+++ twitter-export.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 
 # https://twitter.com/statuses/user_timeline/albertzeyer.xml?page=x
 
--- twitter-search.py
+++ twitter-search.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python2
 
 def usage():
 	print __file__, "<search terms>"
